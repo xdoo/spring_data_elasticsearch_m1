9f06098259d8257f9bf5f94fa64cf34c42899ef2
xdoo <claus.straube@gmail.com>
2019-03-19 10:08:59 +0100

    added suggester to entity

--- src/main/java/de/lhm/elasticsearchtestcase/model/Foo.java
+++ src/main/java/de/lhm/elasticsearchtestcase/model/Foo.java
@@ -1,24 +1,33 @@
 package de.lhm.elasticsearchtestcase.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.CompletionField;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
+import org.springframework.data.elasticsearch.core.completion.Completion;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
+@RequiredArgsConstructor
 @Document(indexName = "foos", type = "foo")
 public class Foo {
-    @Id
+    @Id @NonNull
     String id;
+
+    @NonNull
     @Field(type = FieldType.Text)
     String myfoo;
+
+    @NonNull
     @Field(type = FieldType.Text)
     String title;
+
+    @NonNull
     @Field(type = FieldType.Nested, includeInParent = true)
     Bar bar;
+
+    @CompletionField
+    Completion suggest;
 }
--- src/test/java/de/lhm/elasticsearchtestcase/FooSearchServiceSpringTests.java
+++ src/test/java/de/lhm/elasticsearchtestcase/FooSearchServiceSpringTests.java
@@ -10,6 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
+import org.springframework.data.elasticsearch.core.completion.Completion;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -30,33 +31,49 @@ public class FooSearchServiceSpringTests {
 	public void testSearchForFoo() {
 
 	    // save 4 different foo objects to index 'foos'
+
+        // 01
         Foo foo01 = new Foo(
                 "FOO0001",
                 "xyzfoo",
                 "awesome title",
                 new Bar("qwerbar")
         );
+        Completion c001 = new Completion(new String[]{"xyzfoo", "awesome", "title", "FOO0001", "xyzfoo awesome"});
+        foo01.setSuggest(c001);
         this.fooRepository.save(foo01);
+
+        // 02
         Foo foo02 =new Foo(
                 "FOO0002",
                 "xyzfoo",
                 "poiu title",
                 new Bar("asdfbar")
         );
+        Completion c002 = new Completion(new String[]{"FOO0002", "xyzfoo", "poiu", "title", "poiu title"});
+        foo02.setSuggest(c002);
         this.fooRepository.save(foo02);
+
+        // 03
         Foo foo03 =new Foo(
                 "FOO0003",
                 "poiufoo",
                 "xyztitle",
                 new Bar("qwerbar")
         );
+        Completion c003 = new Completion(new String[]{"FOO0003", "poiufoo", "xyztitle"});
+        foo03.setSuggest(c003);
         this.fooRepository.save(foo03);
+
+        // 04
         Foo foo04 =new Foo(
                 "FOO0004",
                 "poiufoo",
-                null,
+                "Boobel dibubb",
                 new Bar("asdfbar")
         );
+        Completion c004 = new Completion(new String[]{"FOO0004", "poiufoo", "Boobel", "dibubb", "Boobel dibubb"});
+        foo04.setSuggest(c004);
         this.fooRepository.save(foo04);
 
         // check save
